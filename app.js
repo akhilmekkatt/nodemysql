3abc18f6810451083dde08b8b69ac225a5c0460f
akhilmekkatt <akhilmekkatt@gmail.com>
2018-02-10 09:57:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+// Create connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql'
+});
+
+// Connect
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+
+// Create table
+app.get('/createuser', (req, res) => {
+    let sql = 'CREATE TABLE users(id int AUTO_INCREMENT, name VARCHAR(255), address VARCHAR(255), PRIMARY KEY(id))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Posts table created...');
+    });
+});
+
+// Insert post 1
+app.get('/addpost1', (req, res) => {
+    let post = { name: 'User', address: 'This is address of User' };
+    let sql = 'INSERT INTO users SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('user 1 added...');
+    });
+});
+
+// Select posts
+app.get('/getposts', (req, res) => {
+    let sql = 'SELECT * FROM users';
+    let query = db.query(sql, (err, results) => {
+        if (err) throw err;
+        console.log(results);
+        res.send('Posts fetched...');
+    });
+});
+
+// Select single post
+app.get('/getpost/:id', (req, res) => {
+    let sql = `SELECT * FROM users WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post fetched...');
+    });
+});
+
+// Update post
+app.get('/updatepost/:id', (req, res) => {
+    let newTitle = 'Updated Title';
+    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post updated...');
+    });
+});
+
+// Delete post
+app.get('/deletepost/:id', (req, res) => {
+    let newTitle = 'Updated Title';
+    let sql = `DELETE FROM posts WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post deleted...');
+    });
+});
+
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
+});
